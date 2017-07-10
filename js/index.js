f9f7b6785585262d4722b5d419ce15121436fce2
gioulisapo <apg@localhost.localdomain>
2017-07-10 16:07:36 +0100

    partial fix of modals

---
+++ js/index.js
@@ -0,0 +1,99 @@
+$(function() {
+    $('#expand-button').click(function() {
+        $('#expanded').slideToggle();
+        if (document.querySelector('#expand-button').className == "glyphicon glyphicon-collapse-down")
+            document.getElementById("expand-button").className = "glyphicon glyphicon-collapse-up";
+        else
+            document.getElementById("expand-button").className = "glyphicon glyphicon-collapse-down";
+    });
+});
+$(function() {
+    $('#expand-button_2').click(function() {
+        $('#expanded_1').slideToggle();
+        if (document.querySelector('#expand-button_2').className == "glyphicon glyphicon-collapse-down")
+            document.getElementById("expand-button_2").className = "glyphicon glyphicon-collapse-up";
+        else
+            document.getElementById("expand-button_2").className = "glyphicon glyphicon-collapse-down";
+    });
+});
+$(function() {
+    $('#expand-button_3').click(function() {
+        $('#expanded_2').slideToggle();
+        if (document.querySelector('#expand-button_3').className == "glyphicon glyphicon-collapse-down")
+            document.getElementById("expand-button_3").className = "glyphicon glyphicon-collapse-up";
+        else
+            document.getElementById("expand-button_3").className = "glyphicon glyphicon-collapse-down";
+    });
+});
+// Get the modal
+window.onload = function() {
+    var modal = document.getElementById('myModal_1');
+
+    // Get the image and insert it inside the modal - use its "alt" text as a caption
+    var img = document.getElementById('gif_expandable_1');
+    var modalImg = document.getElementById("img01");
+    var captionText = document.getElementById("caption_1");
+    img.onclick = function() {
+        modal.style.display = "block";
+        modalImg.src = this.src;
+        captionText.innerHTML = this.alt;
+    }
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+}
+$(document).keyup(function(e) {
+    var modal = document.getElementById('myModal_1');
+    if (e.keyCode == 27) { // escape key maps to keycode `27`
+        modal.style.display = "none";
+    }
+});
+
+// Get the modal
+window.onload = function() {
+    var modal_1 = document.getElementById('myModal_1');
+    var modal_2 = document.getElementById('myModal_2');
+
+    // Get the image and insert it inside the modal - use its "alt" text as a caption
+    var img_1 = document.getElementById('gif_expandable_1');
+    var modalImg_1 = document.getElementById("img01");
+    var captionText_1 = document.getElementById("caption_1");
+
+    var img_2 = document.getElementById('gif_expandable_2');
+    var modalImg_2 = document.getElementById("img02");
+    var captionText_2 = document.getElementById("caption_2");
+
+    img_1.onclick = function() {
+        modal_1.style.display = "block";
+        modalImg_1.src = this.src;
+        captionText_1.innerHTML = this.alt;
+    }
+    img_2.onclick = function() {
+        modal_2.style.display = "block";
+        modalImg_2.src = this.src;
+        captionText_2.innerHTML = this.alt;
+    }
+
+    // Get the <span> element that closes the modal
+    var span_1 = document.getElementsByClassName("close_1")[0];
+    var span_2 = document.getElementsByClassName("close_2")[0];
+
+    // When the user clicks on <span> (x), close the modal
+    span_1.onclick = function() {
+        modal_1.style.display = "none";
+    }
+    span_2.onclick = function() {
+        modal_2.style.display = "none";
+    }
+}
+$(document).keyup(function(e) {
+    var modal = document.getElementById('myModal_2');
+    if (e.keyCode == 27) { // escape key maps to keycode `27`
+        modal.style.display = "none";
+    }
+});
